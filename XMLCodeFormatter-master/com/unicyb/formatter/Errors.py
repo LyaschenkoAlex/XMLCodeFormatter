435f67a185606acf9050e0bb276ac1b68885a559
LyaschenkoAlex <lyaschenko@knu.ua>
2019-10-20 00:02:30 +0300

    in windows

---
+++ XMLCodeFormatter-master/com/unicyb/formatter/Errors.py
@@ -0,0 +1,155 @@
+import re
+from com.unicyb.formatter.FormatCode import read_from_file
+
+input_file = ""
+
+
+# переход на новую строку > 3 done
+# неправильные пробелы внутри тега  done
+#  = "fwef" inside tag done
+# ajojirjf<fjoiwej> done
+# неправильная табуляция
+# <><><> done
+
+def check_new_lines():
+    tmp_input_file = input_file
+    arr_new_line = re.findall(r"[\n][\n][\n][\n]+", tmp_input_file)
+    ans_arr = []
+    for i in range(len(arr_new_line) - 1, -1, -1):
+        ans_arr.append('<span class="check_NL"><span class="line">line - ' + str(
+            tmp_input_file[:tmp_input_file.rfind(arr_new_line[i])].count('\n') + 2) +
+                       '</span> -> too many NL in a row, max - 3</span>')
+        tmp_input_file = tmp_input_file[:tmp_input_file.rfind(arr_new_line[i])]
+    read_to_file('<!DOCTYPE html><html><head><link rel="stylesheet" href="styles.css"></head><body><p>' + "<br>".join(
+        ans_arr[::-1]), "w")
+
+
+def wrong_spaces_inside_tag():
+    tmp_input_file = input_file
+    arr_inside_tags = re.findall(r"<[^<]+>", tmp_input_file)
+    ans_arr = []
+    for i in range(len(arr_inside_tags) - 1, -1, -1):
+
+        str_tag = arr_inside_tags[i]
+        if str_tag != str_tag.strip() or str_tag != str_tag.replace("  ", " "):
+            str_replaced = str_tag.replace("<", "&lt;")
+            str_replaced = str_replaced.replace(">", "&gt;")
+            str_replaced = str_replaced.replace(" ", "&nbsp;")
+            str_ans = '<span class="inside_tag"><span class="line">line - ' + str(
+                tmp_input_file[:tmp_input_file.rfind(arr_inside_tags[i])].count('\n') + 1) + \
+                      ' -> </span> inside tag &lt;...&gt; wrong whitespaces (' + str_replaced + ')</span>'
+            ans_arr.append(str_ans)
+            tmp_input_file = tmp_input_file[:tmp_input_file.rfind(arr_inside_tags[i])]
+    read_to_file("<br>" + "<br>".join(ans_arr[::-1]), "a+")
+
+
+def equal_sign():
+    tmp_input_file = input_file
+    arr_inside_tags = re.findall(r"<[^<]+>", tmp_input_file)
+    ans_arr = []
+
+    for i in range(len(arr_inside_tags) - 1, -1, -1):
+        arr_inside_tags_whitespace_after = re.findall(r"=[ ]+", arr_inside_tags[i])
+        arr_inside_tags_whitespace_before = re.findall(r"[ ]+=", arr_inside_tags[i])
+        if len(arr_inside_tags_whitespace_before) or len(arr_inside_tags_whitespace_after):
+            str_tag = arr_inside_tags[i]
+            index = tmp_input_file.rfind(str_tag)
+            tmp_input_file = tmp_input_file[:index]
+            num_line = tmp_input_file[:index].count('\n')
+            str_tag = str_tag.replace('<', '&lt;')
+            str_tag = str_tag.replace('>', '&gt;')
+            str_tag = str_tag.replace(' ', '&nbsp;')
+            str_ans = '<span class="equal"> <span class="line">line - ' + str(
+                num_line + 1) + '</span> -> after and before equal sign should not be whitespace (' + str_tag + ')</span>'
+            ans_arr.append(str_ans)
+    read_to_file("<br>" + "<br>".join(ans_arr[::-1]), "a+")
+
+
+def count_tags_in_line():
+    tmp_input_file = input_file
+    arr_new_line = input_file.split('\n')
+    ans_arr = []
+    for i in range(len(arr_new_line) - 1, - 1, - 1):
+        tmp_input_file = tmp_input_file[:tmp_input_file.rfind(arr_new_line[i]) + len(arr_new_line[i])]
+        arr_tags = re.findall(r"<[^<]+>", arr_new_line[i])
+        cnt = 0
+        for j in arr_tags:
+            if j[1:].startswith('!'):
+                cnt += 1
+        if len(arr_tags) - cnt > 2:
+            arr_new_line[i] = arr_new_line[i].replace('<', '&lt;')
+            arr_new_line[i] = arr_new_line[i].replace('>', '&gt;')
+            arr_new_line[i] = arr_new_line[i].strip()
+            arr_new_line[i] = arr_new_line[i].replace(' ', '&nbsp;')
+            str_ans = '<span class="count_tags"> <span class="line">line - ' + str(
+                i + 1) + ' -&gt;</span> too much tags in one line, MAX - 2 (' + \
+                      arr_new_line[i] + ')</span>'
+            ans_arr.append(str_ans)
+    read_to_file("<br>" + "<br>".join(ans_arr[::-1]), "a+")
+
+
+def teg_on_next_line():
+    tmp_input_file = input_file
+    arr_new_line = input_file.split('\n')
+    ans_arr = []
+    for i in range(len(arr_new_line) - 1, - 1, - 1):
+        tmp_input_file = tmp_input_file[:tmp_input_file.rfind(arr_new_line[i]) + len(arr_new_line[i])]
+        arr_tags = re.findall(r"<[^<]+>", arr_new_line[i])
+        cnt = 0
+        for j in arr_tags:
+            if j[1:].startswith('!'):
+                cnt += 1
+        if len(arr_tags) == 1 and '!' not in arr_tags[0]:
+            if arr_new_line[i][:arr_new_line[i].find('<')].replace(' ', '') != '':
+                index_NL = tmp_input_file[:tmp_input_file.rfind(arr_new_line[i])].count('\n')
+                arr_new_line[i] = arr_new_line[i].replace('>', '&gt;')
+                arr_new_line[i] = arr_new_line[i].replace('<', '&lt;')
+                arr_new_line[i] = arr_new_line[i].replace(' ', '&nbsp;')
+                arr_new_line[i] = arr_new_line[i][arr_new_line[i].find('&lt;'):]
+                str_ans = '<span class = "teg_next_line"><span class="line">line - ' + str(
+                    index_NL + 1) + ' -&gt; </span>' + arr_new_line[i] + ' - should be on the next line</span>'
+                ans_arr.append(str_ans)
+    read_to_file("<br>" + "<br>".join(ans_arr[::-1]), "a+")
+
+
+def wrong_tab():
+    tmp_input_file = input_file
+    nesting_level = 0
+    ans_arr = []
+    arr_new_line = input_file.split('\n')
+    arr_tags = []
+    for i in range(0, len(arr_new_line)):
+        nesting_level_upd = nesting_level
+        if arr_new_line[i].lstrip()[1:].startswith('/'):
+            nesting_level_upd -= 1
+        if (nesting_level_upd * 4) * ' ' + arr_new_line[i].lstrip() != arr_new_line[i] and arr_new_line[i] != '':
+            str_ans = '<span class="wrong_tab"><span class="line">line - ' + str(i + 1) + ' -&gt; </span> wrong tab!'
+            ans_arr.append(str_ans)
+        for j in re.findall(r"<[^<!?]+>", arr_new_line[i]):
+            tag = j
+            tag = tag.replace('<', '')
+            tag = tag.replace('>', '')
+            if tag.startswith('/'):
+                nesting_level -= 1
+            elif not tag.endswith('/') and not tag.startswith('!') and not tag.startswith('?'):
+                nesting_level += 1
+
+    read_to_file("<br>" + "<br>".join(ans_arr), "a+")
+
+
+def read_to_file(result_string, state):
+    f = open("../../../resources/outputErrors.html", state)
+    f.write(result_string)
+    f.close()
+
+
+def find_errors():
+    global input_file
+    input_file = read_from_file('../../../resources/input.xml')
+    check_new_lines()
+    wrong_spaces_inside_tag()
+    equal_sign()
+    count_tags_in_line()
+    teg_on_next_line()
+    wrong_tab()
+    read_to_file('</p></body></html>', "a+")
---
+++ XMLCodeFormatter-master/com/unicyb/formatter/FormatCode.py
@@ -0,0 +1,244 @@
+import re
+
+tokens = []
+number_of_brackets = 0  # <  >
+number_of_single_brackets = 0  # '  '
+number_of_double_brackets = 0  # "  "
+text_in_brackets = ""
+input_file = ""
+text_between_brackets = ""
+nesting_level = 0
+result_string = ""
+is_comment = False
+keywords_in_brackets = []
+
+
+def read_from_file(file_name):
+    global input_file
+    input_file = open(file_name).read()
+    input_file = re.sub(r'[\n][\n][\n]+', '\n\n\n', input_file)
+    return open(file_name).read()
+
+
+def get_char_from_file():
+    global input_file
+    for i in input_file:
+        parse(i)
+
+
+def parse(input):
+    if is_comment:
+        in_brackets(input)
+    elif input == "<":
+        less()
+    elif input == ">":
+        greater()
+    elif number_of_brackets == 1:
+        in_brackets(input)
+    elif number_of_brackets == 0:
+        between_brackets(input)
+
+
+def less():
+    global number_of_brackets
+    if number_of_brackets == 0 and number_of_single_brackets == 0 and number_of_double_brackets == 0:
+        global text_between_brackets
+        if text_between_brackets != "":
+            text_between_brackets = re.sub(r'[ ]+', ' ', text_between_brackets)
+            text_between_brackets = re.sub(r'\n[ ]+', '\n', text_between_brackets)
+            text_between_brackets = text_between_brackets.replace('\n', '<br>')
+            if text_between_brackets != "":
+                tokens.append({"between_brackets": text_between_brackets})
+            text_between_brackets = ""
+        number_of_brackets += 1
+        tokens.append({"less": "<"})
+    elif number_of_brackets == 1:
+        in_brackets("<")
+
+
+def greater():
+    global number_of_brackets
+    if number_of_brackets == 1 and number_of_single_brackets == 0 and number_of_double_brackets == 0:
+        number_of_brackets = number_of_brackets - 1
+        global text_in_brackets
+        if text_in_brackets != "":
+            text_in_brackets = text_in_brackets.strip()
+            text_in_brackets = re.sub(r'[ ]+', ' ', text_in_brackets)
+            text_in_brackets = re.sub(r'\n[ ]+', '\n', text_in_brackets)
+            if text_in_brackets != "":
+                tokens.append({"in_brackets": text_in_brackets})
+            tokens.append({"greater": ">"})
+            text_in_brackets = ""
+    elif number_of_brackets == 1:
+        in_brackets(">")
+
+
+def new_line():
+    if number_of_single_brackets == 0 and number_of_double_brackets == 0:
+        tokens.append({"NL": "<br>"})
+    elif number_of_brackets == 1:
+        in_brackets("<br>")
+
+
+def in_brackets(input):
+    global text_in_brackets
+    global number_of_single_brackets
+    global number_of_double_brackets
+    text_in_brackets += input
+
+    global is_comment
+    global number_of_brackets
+    global nesting_level
+
+    if text_in_brackets.startswith('!') and not is_comment:
+        del tokens[-1]
+        number_of_brackets -= 1
+        text_in_brackets = '&lt;' + text_in_brackets
+        is_comment = True
+    if not is_comment:
+        if number_of_double_brackets == 1 and input == '"':
+            number_of_double_brackets -= 1
+        elif number_of_single_brackets == 0 and input == '"' and number_of_double_brackets == 0:
+            number_of_double_brackets += 1
+        if number_of_single_brackets == 1 and input == "'":
+            number_of_single_brackets -= 1
+        elif number_of_single_brackets == 0 and input == "'" and number_of_double_brackets == 0:
+            number_of_single_brackets += 1
+    if text_in_brackets.endswith('-->'):
+        tokens.append({"comment": text_in_brackets.replace('>', '&gt')})
+        text_in_brackets = ""
+        is_comment = False
+
+
+def between_brackets(input):
+    global text_between_brackets
+    text_between_brackets += input
+
+
+def format_element_from_array(array):
+    for i in array:
+        create_formatted_text(i)
+
+
+def create_formatted_text(dict):
+    for tkn, vl in dict.items():
+        if tkn == 'less':
+            formatted_less()
+        elif tkn == 'in_brackets':
+            formatted_in_brackets(vl)
+        elif tkn == 'greater':
+            formatted_greater()
+        elif tkn == 'comment':
+            formatted_comment(vl)
+        elif tkn == 'between_brackets':
+            formatted_between_brackets(vl)
+
+
+def formatted_less():
+    global result_string
+    global nesting_level
+    index_br = result_string.rfind('<br>')
+    index_gt = result_string.rfind('&gt')
+    if index_gt < index_br and result_string[index_br:] != '<br>':
+        result_string += '```'
+        #######################
+    if ((index_br == -1 and result_string.count('&lt;') > 0) or result_string[index_br + 3:].count('&lt;') > 0) \
+            and (result_string.endswith('&gt') or result_string[index_gt + 3].isspace()):
+        result_string += '```'
+    result_string += nesting_level * '&emsp;' + '&lt;'
+    nesting_level += 1
+
+
+def formatted_in_brackets(value):
+    value = re.sub(r'[ ]*=[ ]*', '=', value)
+    global nesting_level
+    global result_string
+    # print(result_string)
+    if result_string.endswith('\n'):
+        value = 4 * nesting_level * ' ' + '    ' + value
+        print('true')
+    if value.startswith('?'):
+        nesting_level -= 1
+    if value.startswith('/'):
+        if nesting_level != 0:
+            index = result_string.rfind('&emsp;')
+            index_br = result_string.rfind('<br>')
+            if result_string[index_br:].count('&lt;') > 1:
+                result_string = result_string[:index - (nesting_level - 2) * 6] + '&lt;'
+            else:
+                result_string = result_string[:index] + '&lt;'
+            nesting_level -= 2
+    if value.endswith('/'):
+        nesting_level -= 1
+    result_string += value
+
+
+def formatted_between_brackets(value):
+    global result_string
+    value = value.strip()
+    value = re.sub(r'<br><br>(<br>)+', '<br><br><br>', value)
+    if value != '<br>':
+        value = re.sub(r'<br>', '<br>' + nesting_level * '&emsp;', value)
+    while value.endswith('&emsp;'):
+        value = value[:len(value) - 6]
+    result_string += value
+
+
+def formatted_greater():
+    global result_string
+    global nesting_level
+    index_of_br = result_string.rfind('<br>')
+    count_of_less = result_string[index_of_br:].count('&lt;')
+    if count_of_less > 1:
+        index_less = result_string.rfind('&lt;')
+        index_less_less = result_string[:index_less].rfind('&lt;')
+        string_less = result_string[index_less_less + 4: index_less]
+        # string_less = string_less.replace('<br>', '')
+        string_less = string_less.replace('&emsp;', '')
+        string_less = string_less.replace('&gt', '')
+        string = result_string[index_less + 4:]
+        string = string.replace('<br>', '')
+        string = string.replace('&emsp;', '')
+        string = string.replace('&gt', '')
+        if string.startswith('/'):
+            try:
+                if string[1:] == string_less.replace('```', '') or (string_less.startswith('!') and
+                                                                    string_less.replace('```', '') != string_less):
+                    index_greater = result_string[:index_less].rfind('&gt')
+                    result_string = result_string[:index_greater] + '&gt' + '&lt;' + string
+
+                else:
+                    result_string = result_string.replace('```', '<br>' + nesting_level * '&emsp;')
+            except:
+                print('exception - 201')
+        elif string.startswith('!'):
+            result_string = result_string.replace('```', '<br>' + nesting_level * '&emsp;')
+            # print('comment')
+        else:
+            result_string = result_string.replace('```', '<br>')
+            result_string += '&gt'
+    else:
+        result_string = result_string.replace('```', '<br>')
+    result_string += '&gt'
+
+
+def formatted_comment(value):
+    global result_string
+    if result_string.endswith('<br>'):
+        result_string = result_string + nesting_level * '&emsp;'
+    result_string += value
+
+
+def start_format():
+    global result_string
+    read_from_file('../../../resources/input.xml')
+    get_char_from_file()
+    format_element_from_array(tokens)
+    result_string = result_string.replace('&gt&gt', '&gt')
+    result_string = result_string.replace('&gt', '<span class="bracket">&gt</span>')
+    result_string = result_string.replace('&lt;', '<span class="bracket">&lt;</span>')
+    while result_string.startswith('<br>'):
+        result_string = result_string[4:]
+    result_string = '<!DOCTYPE html><html><head><link rel="stylesheet" href="styles.css"></head><body><p>' \
+                    + result_string + '</p></body></html>'
+    return result_string
